d98f09a34861952e37c5f2e3b5664b3710fdc6d7
马振坤 <mazhenkun@qianlima.com>
2022-09-08 10:29:22 +0800

    Changes

--- test.py
+++ test.py
@@ -14,7 +14,8 @@ class Employee():
         self.xian = xian
 
     def print_hi():
-        xlsx = xlrd2.open_workbook('D:/合约商机线上数据-给到振坤.xlsx')
+        # xlsx = xlrd2.open_workbook('D:/合约商机线上数据-给到振坤.xlsx')
+        xlsx = xlrd2.open_workbook('D:/A-全国物业注册地址-9.2.xlsx')
         #  ExcelReader reader2 = ExcelUtil.getReader("D:/合约商机线上数据-给到振坤1.xlsx", 2);
         # 通过sheet名查找：xlsx.sheet_by_name("sheet1")
         # 通过索引查找：xlsx.sheet_by_index(3)
@@ -28,7 +29,7 @@ class Employee():
         nrows = table.nrows
         print("表格一共有", nrows, "行")
         list1 = []
-        # 获取第4列所有值（列表生成式）
+        # 获取第1列所有值（列表生成式）
         name_list = [str(table.cell_value(i, 0)) for i in range(1, nrows)]
         # print("第4列所有的值：", name_list)
         for i in name_list:
@@ -44,15 +45,8 @@ class Employee():
             else:
                 # print("序号：%s   值：%s 分词结果：%s" % (name_list.index(i) + 1, i, df))
                 a = df.to_dict('dict')
-                # print(a)
-                # print(list(a.get("省").values())[0])
-                # print(list(a.get("市").values())[0])
-                # print(list(a.get("区").values())[0])
                 emp1 = Employee(i, list(a.get("省").values()), list(a.get("市").values()), list(a.get("区").values()))
                 list1.append(emp1)
-
-        file = xlwt.Workbook('encoding = utf-8')
-
         # 创建sheet工作表
         # sheet1 = file.add_sheet('sheet1', cell_overwrite_ok=True)
         outwb = openpyxl.Workbook()
@@ -63,45 +57,17 @@ class Employee():
         outws.cell(1, 1).value = "内容"  # 第1行第1列
         outws.cell(1, 2).value = "省"  # 第1行第2列
         outws.cell(1, 3).value = "市"  # 第1行第3列
-        outws.cell(1, 4).value = "区"  # 第1行第3列
-        #  sheet1.write(0, 0, "内容")  # 第1行第1列
-        # sheet1.write(0, 1, "省")  # 第1行第2列
-        # sheet1.write(0, 2, "市")  # 第1行第3列
-        # sheet1.write(0, 3, "区")  # 第1行第3列
+        outws.cell(1, 4).value = "区"  # 第1行第4列
         # 循环填入数据
         for i in range(len(list1)):
-            outws.cell(i + 2, 1).value = list1[i].content  # 第1列序号
-            outws.cell(i + 2, 2).value = str(list1[i].sheng)  # 第2列数量
-            outws.cell(i + 2, 3).value = str(list1[i].shi)  # 第2列数量
-            outws.cell(i + 2, 4).value = str(list1[i].xian)  # 第2列数量
-            # if list1[i].sheng is None:
-            #     outws.cell(i + 1, 1).value = "null"  # 第2列数量
-            # else:
-            #     if list1[i].sheng is None:
-            #         outws.cell(i + 1, 1, "null")  # 第2列数量
-            #     else:
-            #         outws.cell(i + 1, 1, list1[i].sheng[0])  # 第2列数量
-            # print(list1[i].shi)
-            # if list1[i].shi is None:
-            #     outws.cell(i + 1, 2, "null")  # 第2列数量
-            # else:
-            #     if list1[i].shi[0] is None:
-            #         outws.cell(i + 1, 2, "null")  # 第2列数量
-            #     else:
-            #         outws.cell(i + 1, 2, list1[i].shi[0])  # 第2列数量
-            # if list1[i].xian is None:
-            #     outws.cell(i + 1, 3, "null")  # 第2列数量
-            # else:
-            #     if list1[i].xian[0] is None:
-            #         outws.cell(i + 1, 3, "null")  # 第2列数量
-            #     else:
-            #         outws.cell(i + 1, 3, list1[i].xian[0])  # 第2列数量
-            # sheet1.write(i + 1, 1, list1[i].shi)  # 第3列误差
-            # sheet1.write(i + 1, 1, list1[i].xian)  # 第3列误差
-        # 保存Excel到.py源文件同级目录
-        # outwb.save(saveExcel)
-        outwb.save('Data.xls')
+            outws.cell(i + 2, 1).value = list1[i].content  # 第1列
+            outws.cell(i + 2, 2).value = str(list1[i].sheng[0])  # 第2列
+            outws.cell(i + 2, 3).value = str(list1[i].shi[0])  # 第3列
+            outws.cell(i + 2, 4).value = str(list1[i].xian[0])  # 第4列
+        outwb.save('A-全国物业注册地址-9.2结果1.xls')
 
 
 if __name__ == '__main__':
-    Employee.print_hi()
+    df = cpca.transform_text_with_addrs("广东省深圳市龙华区")
+    print(df)
+    # Employee.print_hi()
